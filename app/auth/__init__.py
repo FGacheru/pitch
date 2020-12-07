6fb6220e17d8756d59f899089b3689f8f0d2b83d
b-ndunda <francisgacheru2001@gmail.com>
2020-12-07 16:54:34 +0300

    initialize bootstrap

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,11 +2,7 @@ from flask import Blueprint
 
 auth = Blueprint('auth',__name__)
 
-from . import views
+from . import views,forms
 
 
 
-def create_app(config_name):
-    #...
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
