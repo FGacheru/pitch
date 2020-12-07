323edeed57df5082574cd2388249712ab6f0612f
b-ndunda <francisgacheru2001@gmail.com>
2020-12-07 16:52:23 +0300

    add SQLALCHEMY url

--- config.py
+++ config.py
@@ -1,10 +1,24 @@
 import os
 
+#  email configurations
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 class Config:
     """
     general configuration parent class
     """
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:23646668@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
     
     
 class ProdConfig(Config):
@@ -26,13 +40,17 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings.
     """
-    DEBUG = True    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:23646668@localhost/pitches'
+    DEBUG = True   
     
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:23646668@localhost/pitches_test'
     
-    
+       
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }    
     
     
